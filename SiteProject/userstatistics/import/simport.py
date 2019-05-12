2671b34d887a993a8a8bad38fffccad6ab852cce
Pavel Kotolevsky <pas199797@gmail.com>
2019-05-12 16:34:53 +0300

    123

--- SiteProject/userstatistics/import/simport.py
+++ SiteProject/userstatistics/import/simport.py
@@ -5,42 +5,50 @@ from django.contrib.auth.models import User
 HEADER_SKIP_COLS = 1
 
 class Import:
+	def get_header(self,sheet):
+		head = []
+		for col_index in range(HEADER_SKIP_COLS,sheet.ncols):
+			val = sheet.cell(0, col_index).value
+			if (isinstance(val, float) and round(val) == val):
+				head.append(str(round(val)))
+			elif (not isinstance(val, str)):
+				head.append(str(val))
+			else:
+				head.append(val)
+		return head
+	#return block from fitst empty line to next empty line 
+	def get_block(self,sheet, start_row_num = 0):
+		if (start_row_num != 0):
+			crr_val = sheet.cell_value(start_row_num,0)
+			while (crr_val != u'' and start_row_num<sheet.nrows-1):
+				start_row_num+=1
+				crr_val = sheet.cell_value(start_row_num,0)
+
+		rows = []
+		crr_row_num = start_row_num
+		crr_val = u'-'
+		while(crr_val != u'' and crr_row_num<sheet.nrows-1):
+			crr_row_num+=1
+			row = sheet.row_values(crr_row_num)
+			crr_val = row[0]
+			if (crr_val!=u''):
+				rows.append(row)
+		end_block = not crr_row_num < sheet.nrows-1
+		return end_block,crr_row_num,rows
+
 	def import_xls(self, path):
 		#open file
 		wb = xlrd.open_workbook(file_contents = path.read())
 		if (wb):
 			sheet = wb.sheet_by_index(0)
 			if (sheet):
-				#read header
-				head = []
-				for col_index in range(HEADER_SKIP_COLS,sheet.ncols):
-					val = sheet.cell(0, col_index).value
-					if (isinstance(val, float) and round(val) == val):
-						head.append(str(round(val)))
-					elif (not isinstance(val, str)):
-						head.append(str(val))
-					else:
-						head.append(val)
-				#create tasks if not exist
-				tasks = []
-				for col in head:
-					tasks.append(Task.objects.get_or_create(name = col)[0])
-				
-				for row_num in range(1,sheet.nrows):
-					row = sheet.row_values(row_num)
-					user_exist = False
-					if (row[0] != u'' and isinstance(row[0],str)):
-						if ('crr_job' not in locals()):
-							crr_job = Job.objects.create(year = 2019) #TODO insert year
-						#try get user
-						names = row[0].split()
-						while(len(names)<3):
-							names.append(u'')
-						try:
-							crr_usr = User.objects.get(first_name = names[1], last_name = names[0], profile__patronymic = names[2])
-							user_exist = True
-						except DoesNotExist:
-							user_exist = False
-
-
-		return
+				head = self.get_header(sheet)
+				#read jobs from file
+				end = False
+				next_job_num = 0
+				err_jobs = []
+				while (not end):
+					crr_job_num = next_job_num
+					end,next_job_num,job_block = self.get_block(sheet, next_job_num)
+					if not (Job.import_job(head,job_block,abort_if_user_not_exists = False)):
+						err_jobs.append(crr_job_num)
--- SiteProject/userstatistics/models.py
+++ SiteProject/userstatistics/models.py
@@ -1,12 +1,7 @@
-from django.db import models
-
-# Create your models here.
-
-from django.db import models
+from django.db import models, transaction, IntegrityError
 from django.contrib.auth.models import User
 from profiles.customfields import IntegerRangeField
-
-# Create your models here.
+from django.core.exceptions import ObjectDoesNotExist
 
 class Task(models.Model):
 	name = models.CharField(max_length=20, help_text="Введите название задачи", unique = True)
@@ -26,11 +21,68 @@ class Job(models.Model):
 	def __str__(self):
 		return self.appraiser.username
 
+	
+
+	def import_job(tasks, scores, create_users = False, abort_if_user_not_exists = True):
+		#create or get users
+		users = []
+		for row in scores:
+			names = row[0].split()
+			while(len(names)<3):
+					names.append(u'')
+			try:
+				crr_usr = User.objects.get(first_name = names[1], last_name = names[0], profile__patronymic = names[2])
+				users.append(crr_usr)
+			except ObjectDoesNotExist:
+				users.append(None)
+			#TODO if need create
+		if (users.count(None)!=0 and abort_if_user_not_exists):
+			return False
+		#create job for job field
+		db_job = Job(year = 2019)
+		#create or get tasks for task field
+		db_tasks = []
+		for col in tasks:
+			try:
+				db_tasks.append(Task.objects.get(name = col))
+			except ObjectDoesNotExist:
+				db_tasks.append(Task(name = col))
+		#create scores
+		db_scores = []
+		for i in range(0,len(scores)):
+			for j in range(1,len(row)):
+				if (isinstance(scores[i][j],float) or isinstance(scores[i][j],int)):
+					db_scores.append(Score(appraiser = users[i],job = db_job, task = db_tasks[j-1],score = scores[i][j],quality = '='))
+		#scores quality
+		if (len(users) == 3):
+			for score in db_scores:
+				if (score.appraiser == users[2]):
+					for slave_score in db_scores:
+						if (slave_score.task == score.task):
+							if (slave_score.score < score.score):
+								slave_score.quality = '-'
+							elif (slave_score.score > score.score):
+								slave_score.quality = '+'
+		#save job in one transaction
+		try:
+			with transaction.atomic():
+				for user in users:
+					user.save()
+				db_job.save()
+				for task in db_tasks:
+					task.save()
+				for score in db_scores:
+					if (score.appraiser != None):
+						score.save()
+			return True
+		except IntegrityError:
+			return False
+
 class Score(models.Model):
 	appraiser = models.ForeignKey(User,on_delete=models.CASCADE)
+	job = models.ForeignKey(Job,on_delete=models.CASCADE)
 	task = models.ForeignKey(Task,on_delete=models.CASCADE)
 	score = IntegerRangeField(min_value=0, max_value=100)
-	job = models.ForeignKey(Job,on_delete=models.CASCADE)
 	QUALITY_CHOICES = (('+','Завысил'),('-','Занизил'),('=','Не пошла на третью проверку'))
 	quality = models.CharField(max_length=1,choices=QUALITY_CHOICES)
 
